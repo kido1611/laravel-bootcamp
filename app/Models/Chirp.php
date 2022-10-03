98fbd378a334cc80b1ac7372c7c0ecc719595228
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2022-10-03 19:27:08 +0700

    Add notifications and events

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,12 +2,17 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Chirp extends Model
 {
     use HasFactory;
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class
+    ];
 
     protected $fillable = [
         'message'
